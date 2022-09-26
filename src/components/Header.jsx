617ca9132d40e7e1a6ce6a6256b5d96780bc03c3
whlshy <whl93187@gmail.com>
2022-09-26 22:35:45 +0800

    Day 10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
   render() {
     return (
       <div className="header">
-            <h3>{this.props.title}</h3>
+        <h3>{this.props.title}</h3>
+        <p
+          onClick={
+            e =>
+              this.setState({ count: this.state.count + 1 })
+          }
+        >
+          {this.state.count}
+        </p>
       </div>
     )
   }
