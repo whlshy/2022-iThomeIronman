56de0b74aada76c4b5a6645d91fa8908abc5c048
WHL <whl93187@gmail.com>
2022-09-30 23:31:23 +0800

    Day 15

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import FocusExample from './components/FocusExample';
 import Header from './components/Header'
+import React from 'react'
 
 function App() {
 
   return (
     <div className="App">
       <Header title="影片標記系統" />
-      <FocusExample />
+      {/* <FocusExample /> */}
     </div>
   );
 }
